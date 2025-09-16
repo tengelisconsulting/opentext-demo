73f6b4175949e4c58ff2fd7d67abf1f890ae647d
Liam <liam@tengelisconsulting.com>
2025-09-16 08:25:59 -0700

    failing

--- demoapp/db/products.js
+++ demoapp/db/products.js
@@ -3,8 +3,8 @@ export function searchProducts () {
     let criteria = req.query.q === 'undefined' ? '' : req.query.q ?? '';
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200);
     models.sequelize
-      .query("SELECT * FROM Products")
-      // .query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
+      // .query("SELECT * FROM Products")
+      .query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
       .then(([products]) => {
         const dataString = JSON.stringify(products);
         for (let i = 0; i < products.length; i++) {
